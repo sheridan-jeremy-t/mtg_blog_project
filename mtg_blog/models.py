079c0b6259a828ce8098da418cf43cef2deeb95e
sheridan-jeremy-t <tempestj@sheridancollege.ca>
2025-07-02 20:38:06 -0400

    Updated models.py with Comment class

--- mtg_blog/models.py
+++ mtg_blog/models.py
@@ -53,3 +53,17 @@ class Post(models.Model):
 
     class Meta:
         ordering = ['-created']
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    text = models.TextField(max_length=500)
+    approved = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post.title}'
+
+    class Meta:
+        ordering = ['-created']
