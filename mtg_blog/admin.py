c9167b6b8029006a0dc7b1e29c0943229cacc285
sheridan-jeremy-t <tempestj@sheridancollege.ca>
2025-07-02 20:06:50 -0400

    updated admin.py

--- mtg_blog/admin.py
+++ mtg_blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Post, Topic
 
-# Register your models here.
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ('name','slug')
+    prepopulated_fields = {'slug':('name',)}
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created', 'updated', 'status', 'author')
+    list_filter = ('status', 'topics')
+    search_fields = ('title', 'author__username', 'author__first_name', 'author__lastname')
+    prepopulated_fields = {'slug':('title',)}
+    ordering = ('created',)
+    filter_horizontal = ('topics',)
