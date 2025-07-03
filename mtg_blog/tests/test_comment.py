9ba4dd9f1127ff490aa1d860817b92409ef3cfac
sheridan-jeremy-t <tempestj@sheridancollege.ca>
2025-07-02 20:32:40 -0400

    created comment tests

---
+++ mtg_blog/tests/test_comment.py
@@ -0,0 +1,60 @@
+import pytest
+from django.test import TestCase
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from django.utils import timezone
+from mtg_blog.models import Comment
+
+class TestCommentModel(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username= 'mtgplayer',
+            email= 'player@example.com',
+            password = 'manaburn123',
+        )
+        self.post = Post.objects.create(
+            title = 'Modern Horizons 3 Card Review',
+            author = self.user,
+            slug = 'modern-horizons-3-review',
+        )
+    def test_comment_creation(self):
+        """Test comment model creation"""
+        comment = Comment.objects.create(
+            post = self.post,
+            name = 'Commander Player',
+            email = 'commander@example.com',
+            text = 'Great review! This card will be amazing in my Eldrazi deck!',
+            approved = True
+        )
+        self.assertEqual(comment.post, self.post)
+        self.assertEqual(comment.name, 'Commander Player')
+        self.assertEqual(comment.approved, True)
+        self.assertIsNotNone(comment.created)
+
+    def test_comment_str_method(self):
+        """Test Comment string representation"""
+        comment = Comment.objects.create(
+            post = self.post,
+            name = 'Standard Player',
+            email = 'standard@example.com',
+            text = 'This will shake up the meta!',
+        )
+        expected_str = f'Comment by Standard Player on {self.post.title}'
+        self.assertEqual(str(comment), expected_str)
+    def test_comment_default_ordering(self):
+        """Test comment default ordering by newest first"""
+        comment1 = Comment.objects.create(
+            post = self.post,
+            name = 'First Commenter',
+            email = 'first@example.com',
+            text = 'First comment about sideboard choices'
+        )
+        comment2 = Comment.objects.create(
+            post=self.post,
+            name='Second Commenter',
+            email='second@example.com',
+            text='Second comment about sideboard choices'
+        )
+        comments = Comment.objects.all()
+        self.assertEqual(comments[0], comment2) #most recent first
+        self.assertEqual(comments[1], comment1)
