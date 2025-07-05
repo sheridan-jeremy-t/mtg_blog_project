447d9ac22d6b312085a880f56821a3a2cb4a5371
sheridan-jeremy-t <tempestj@sheridancollege.ca>
2025-07-05 09:00:56 -0400

    fixed errors in creating superuser and migrations

--- mtg_blog/admin.py
+++ mtg_blog/admin.py
@@ -6,6 +6,12 @@ class TopicAdmin(admin.ModelAdmin):
     list_display = ('name','slug')
     prepopulated_fields = {'slug':('name',)}
 
+class CommentInline(admin.TabularInline):
+    model = Comment
+    fields = ('name','email', 'text', 'approved')
+    readonly_fields = ('name', 'email', 'text')
+    extra = 0
+
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'created', 'updated', 'status', 'author')
@@ -14,30 +20,10 @@ class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('title',)}
     ordering = ('created',)
     filter_horizontal = ('topics',)
-
-class CommentInline(admin.TabularInline):
-    model = Comment
-    fields = ('name','email', 'text', 'approved')
-    readonly_fields = ('name', 'email', 'text')
-    extra = 0
+    inlines = [CommentInline]
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('name', 'post', 'created', 'approved')
     list_filter = ('approved', 'created')
     search_fields = ('name', 'email', 'text')
-
-@admin.register(Topic)
-class TopicAdmin(admin.ModelAdmin):
-    list_display = ('name', 'slug')
-    prepopulated_fields = {'slug':('name',)}
-
-@admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'created', 'updated', 'status', 'author')
-    list_filter = ('status', 'topics')
-    search_fields = ('title', 'author__username', 'author__first_name', 'author__last_name')
-    prepopulated_fields = {'slug':('title',)}
-    ordering = ('created',)
-    filter_horizontal = ('topics',)
-    inlines = [CommentInline]
